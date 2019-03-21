f00c0ad65c5b1f77959aa039d918db6f50edfc0c
haylie.evers <haylie.evers@gmail.com>
2019-03-20 21:51:45 -0500

    Added in some comments to the code

--- src/com/haylieevers/Main.java
+++ src/com/haylieevers/Main.java
@@ -1,33 +1,31 @@
 package com.haylieevers;
 
-import jdk.swing.interop.SwingInterOpUtils;
-
-import javax.swing.*;
-
 public class Main {
-
+    /**
+     * Test The UberDriver class
+     */
     public static void main(String[] args) {
         UberDriver d = new UberDriver(1.00, .20);
         d.drive(5, 15);
-        totalStats(d);
+        totalStats(d); //5, 15, 0.0, 0
         d.pickUp();
         d.drive(6, 3);
-        totalStats(d);
+        totalStats(d); //11, 18, 6.6, 1
         for (int i = 0; i < 5; i++) {
             d.pickUp();
         }
         d.waitAround(2);
-        totalStats(d);
+        totalStats(d); //11, 20, 8.2, 4
         d.dropOff();
         d.driveAtSpeed(3, 25);
-        totalStats(d);
+        totalStats(d); //14, 27, 21.4, 3
         for (int i = 0; i < 10; i++) {
             d.dropOff();
         }
         d.drive(1, 1);
-        totalStats(d);
-        System.out.println(d.getProfit());
-        System.out.println(d.getAverageProfitPerHour());
+        totalStats(d); //15, 28, 21.4, 0
+        System.out.println(d.getProfit()); //13.9
+        System.out.println(d.getAverageProfitPerHour()); //6.48667
         d.driveAtSpeed(80, -63);
 
     }
--- src/com/haylieevers/UberDriver.java
+++ src/com/haylieevers/UberDriver.java
@@ -21,22 +21,23 @@ public class UberDriver {
         perMinuteRate = givenPerMinuteRate;
         passengers = 0;
         totalCredits = 0;
-        }
+    }
 
-    public int getTotalMiles(){
+    //Returns total miles
+    public int getTotalMiles() {
         return milesTotal;
     }
 
 
     //Returns the total minutes driven since this UberDriver was constructed.
-    public int getTotalMinutes(){
+    public int getTotalMinutes() {
         return minutesTotal;
     }
 
 
     //Drives the vehicle for the given number of miles over the given number of minutes.
-    public void drive(int miles, int minutes){
-        milesTotal +=  miles;
+    public void drive(int miles, int minutes) {
+        milesTotal += miles;
         minutesTotal += minutes;
         totalCredits += (miles * perMileRate + minutes * perMinuteRate) * passengers;
     }
@@ -44,60 +45,57 @@ public class UberDriver {
 
     //Simulates sitting in the vehicle without moving for the given number of minutes.
     //      Equivalent to drive(0, minutes).
-    public void waitAround(int minutes)
-    {
+    public void waitAround(int minutes) {
         drive(0, minutes);
     }
 
     //Drives the vehicle for the given number of miles at the given speed. Equivalent to
     //drive(miles, m), where m is the actual number of minutes required, rounded to the
     //nearest integer. Caller of method must ensure that averageSpeed is positive.
-    public void driveAtSpeed(int miles, double averageSpeed){
-        if (averageSpeed <= 0){
+    public void driveAtSpeed(int miles, double averageSpeed) {
+        if (averageSpeed <= 0) {
             throw new RuntimeException("Invalid input, average speed must be positive.  You entered " + averageSpeed);
         }
-          int minutes = (int) Math.round(60 * miles / averageSpeed);
+        int minutes = (int) Math.round(60 * miles / averageSpeed);
 
         drive(miles, minutes);
 
     }
 
     //Returns the number of passengers currently in the vehicle.
-    public int getPassengerCount(){
+    public int getPassengerCount() {
         return passengers;
     }
 
     //Increases the passenger count by 1, not exceeding MAX_PASSENGERS.
-    public void pickUp(){
+    public void pickUp() {
         passengers++;
         passengers = Math.min(passengers, MAX_PASSENGERS);
     }
 
     //Decreases the passenger count by 1, not going below zero.
-    public void dropOff(){
+    public void dropOff() {
         passengers--;
         passengers = Math.max(passengers, 0);
     }
 
     //Returns this UberDriver's total credits (money earned before deducting operating costs).
-    public double getTotalCredits(){
+    public double getTotalCredits() {
         return totalCredits;
     }
 
     //Returns this UberDriver's profit (total credits, less operating costs).
-    public double getProfit(){
+    public double getProfit() {
         double profit = totalCredits - OPERATING_COST * milesTotal;
         return profit;
     }
 
     //Returns this UberDriver's average profit per hour worked. Caller of method must ensure
     //that it is only called when the value of getTotalMinutes() is nonzero.
-    public double getAverageProfitPerHour(){
+    public double getAverageProfitPerHour() {
         double averageProfit = getProfit() * minutesTotal / 60;
         return averageProfit;
     }
 
 
-
 }
-
